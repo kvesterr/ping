6af8461f5919f41398dcac05a35fb74d59e61f64
kvesterr <56050759+kvesterr@users.noreply.github.com>
2022-02-08 15:14:18 +0300

    Update ping.cpp

--- ping.cpp
+++ ping.cpp
@@ -1,6 +1,6 @@
 /*  Ping Project.  */
 
-/*  Подключение библиотек.  */
+/*  РџРѕРґРєР»СЋС‡РµРЅРёРµ Р±РёР±Р»РёРѕС‚РµРє.  */
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <iostream>
@@ -9,12 +9,12 @@
 #include <chrono>
 #include <time.h>
 
-/*  Прочие объявления.  */
+/*  РџСЂРѕС‡РёРµ РѕР±СЉСЏРІР»РµРЅРёСЏ.  */
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
-/*  Глобальные переменные и константы.  */
+/*  Р“Р»РѕР±Р°Р»СЊРЅС‹Рµ РїРµСЂРµРјРµРЅРЅС‹Рµ Рё РєРѕРЅСЃС‚Р°РЅС‚С‹.  */
 
 SOCKET sock = INVALID_SOCKET;
 struct sockaddr_storage t_addr;
@@ -25,41 +25,41 @@ int iResult = 0;
 
 struct sockaddr s_address;
 
-//  Структура описывающая ICMP-пакет.
+//  РЎС‚СЂСѓРєС‚СѓСЂР° РѕРїРёСЃС‹РІР°СЋС‰Р°СЏ ICMP-РїР°РєРµС‚.
 struct icmp_packet
 {
-	//  Заголовок пакета.
-	char type;  //  Тип сообщения, 0 или 8.
-	char code;  //  Код ошибки.
+	//  Р—Р°РіРѕР»РѕРІРѕРє РїР°РєРµС‚Р°.
+	char type;  //  РўРёРї СЃРѕРѕР±С‰РµРЅРёСЏ, 0 РёР»Рё 8.
+	char code;  //  РљРѕРґ РѕС€РёР±РєРё.
 	short control_sum;
 	short id;
-	short seq;  //  Очередь.
+	short seq;  //  РћС‡РµСЂРµРґСЊ.
 
-	//  Данные пакета.
+	//  Р”Р°РЅРЅС‹Рµ РїР°РєРµС‚Р°.
 	const char data[33] = "abcdefghijklmnopqrstuvwabcdefghi";
 };
 
 
-/*  Вспомогательные функцкии.  */
+/*  Р’СЃРїРѕРјРѕРіР°С‚РµР»СЊРЅС‹Рµ С„СѓРЅРєС†РєРёРё.  */
 
-/*  Функция перевода int в двоичный string.  */
+/*  Р¤СѓРЅРєС†РёСЏ РїРµСЂРµРІРѕРґР° int РІ РґРІРѕРёС‡РЅС‹Р№ string.  */
 string to_binary_string(unsigned int n)
 {
-	string buffer; // символы ответа в обратном порядке
-	// выделим память заранее по максимуму
+	string buffer; // СЃРёРјРІРѕР»С‹ РѕС‚РІРµС‚Р° РІ РѕР±СЂР°С‚РЅРѕРј РїРѕСЂСЏРґРєРµ
+	// РІС‹РґРµР»РёРј РїР°РјСЏС‚СЊ Р·Р°СЂР°РЅРµРµ РїРѕ РјР°РєСЃРёРјСѓРјСѓ
 	buffer.reserve(numeric_limits<unsigned int>::digits);
 	do
 	{
-		buffer += char('0' + n % 2); // добавляем в конец
+		buffer += char('0' + n % 2); // РґРѕР±Р°РІР»СЏРµРј РІ РєРѕРЅРµС†
 		n = n / 2;
 	} while (n > 0);
 	buffer = string(buffer.crbegin(), buffer.crend());
 	while (buffer.length() < 8)
 		buffer = '0' + buffer;
-	return buffer; // разворачиваем результат
+	return buffer; // СЂР°Р·РІРѕСЂР°С‡РёРІР°РµРј СЂРµР·СѓР»СЊС‚Р°С‚
 }
 
-/*  Функция перевода из двоичного значения string в десятичное short.  */
+/*  Р¤СѓРЅРєС†РёСЏ РїРµСЂРµРІРѕРґР° РёР· РґРІРѕРёС‡РЅРѕРіРѕ Р·РЅР°С‡РµРЅРёСЏ string РІ РґРµСЃСЏС‚РёС‡РЅРѕРµ short.  */
 short bin_to_short(string value)
 {
 	short result = 0;
@@ -72,19 +72,19 @@ short bin_to_short(string value)
 	return result;
 }
 
-/*  Функция подсчета контрольной суммы.  */
+/*  Р¤СѓРЅРєС†РёСЏ РїРѕРґСЃС‡РµС‚Р° РєРѕРЅС‚СЂРѕР»СЊРЅРѕР№ СЃСѓРјРјС‹.  */
 int control_sum(icmp_packet packet)
 {
 	int sum = 0;
 
-	//  Составление пары type&code в 16 бит.
+	//  РЎРѕСЃС‚Р°РІР»РµРЅРёРµ РїР°СЂС‹ type&code РІ 16 Р±РёС‚.
 	string pair = to_binary_string(packet.type) + to_binary_string(packet.code);
 	sum += bin_to_short(pair);
 	sum += _byteswap_ushort(packet.id);  // 0x0100 -> 0x0001
 	sum += _byteswap_ushort(packet.seq);
 
 	pair = "";
-	//  Составление пар из значений поля данных.
+	//  РЎРѕСЃС‚Р°РІР»РµРЅРёРµ РїР°СЂ РёР· Р·РЅР°С‡РµРЅРёР№ РїРѕР»СЏ РґР°РЅРЅС‹С….
 	for (int i = 0; i < 32; i++)
 	{
 		pair += to_binary_string(packet.data[i]);
@@ -96,14 +96,14 @@ int control_sum(icmp_packet packet)
 		}
 	}
 	
-	//  Корректировка результата под 16 бит.
+	//  РљРѕСЂСЂРµРєС‚РёСЂРѕРІРєР° СЂРµР·СѓР»СЊС‚Р°С‚Р° РїРѕРґ 16 Р±РёС‚.
 	while (sum > 0xffff)
 		sum = (sum & 0xffff) + (sum >> 16);
 
 	return _byteswap_ushort(~sum);
 }
 
-/*  Функция формирования ICMP пакета.  */
+/*  Р¤СѓРЅРєС†РёСЏ С„РѕСЂРјРёСЂРѕРІР°РЅРёСЏ ICMP РїР°РєРµС‚Р°.  */
 icmp_packet fill_icmp_packet()
 {
 	icmp_packet packet;
@@ -116,20 +116,20 @@ icmp_packet fill_icmp_packet()
 	return packet;
 }
 
-/*  Функция преобразования char = 8 => string = "8".  */
+/*  Р¤СѓРЅРєС†РёСЏ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ char = 8 => string = "8".  */
 string ch_to_str(unsigned char val)
 {
 	string res = "";
 	char chislo;
 
-	//  Обработка едениц.
+	//  РћР±СЂР°Р±РѕС‚РєР° РµРґРµРЅРёС†.
 	chislo = val % 10;
 	val = val - chislo;
 	chislo += 48;
 	if (chislo != '0')
 		res = res + chislo;
 
-	//  Обработка десятков.
+	//  РћР±СЂР°Р±РѕС‚РєР° РґРµСЃСЏС‚РєРѕРІ.
 	chislo = (val % 100);
 	val = val - chislo;
 	chislo /= 10;
@@ -137,7 +137,7 @@ string ch_to_str(unsigned char val)
 	if (chislo != '0')
 		res = chislo + res;
 
-	//  Обработка сотен.
+	//  РћР±СЂР°Р±РѕС‚РєР° СЃРѕС‚РµРЅ.
 	chislo = (val % 1000);
 	chislo /= 100;
 	chislo += 48;
@@ -147,7 +147,7 @@ string ch_to_str(unsigned char val)
 	return res;
 }
 
-/*  Функция представляющая строку в виде массива из 4-х элементов  */
+/*  Р¤СѓРЅРєС†РёСЏ РїСЂРµРґСЃС‚Р°РІР»СЏСЋС‰Р°СЏ СЃС‚СЂРѕРєСѓ РІ РІРёРґРµ РјР°СЃСЃРёРІР° РёР· 4-С… СЌР»РµРјРµРЅС‚РѕРІ  */
 unsigned char* ip_to_array(string ip, unsigned char* arr)
 {
 	unsigned char val = 0;
@@ -159,7 +159,7 @@ unsigned char* ip_to_array(string ip, unsigned char* arr)
 			val = (val * 10) + ((int)ip[i]) - 48;
 		else
 		{
-			arr[cntr] = (char)val;  //  Сделать корректную обработку ip адресов.
+			arr[cntr] = (char)val;  //  РЎРґРµР»Р°С‚СЊ РєРѕСЂСЂРµРєС‚РЅСѓСЋ РѕР±СЂР°Р±РѕС‚РєСѓ ip Р°РґСЂРµСЃРѕРІ.
 			val = 0;
 			cntr++;
 		}
@@ -169,7 +169,7 @@ unsigned char* ip_to_array(string ip, unsigned char* arr)
 	return arr;
 }
 
-/*  Функция преобразования ip из структуры SOCKADDR_STORAGE в string.  */
+/*  Р¤СѓРЅРєС†РёСЏ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ ip РёР· СЃС‚СЂСѓРєС‚СѓСЂС‹ SOCKADDR_STORAGE РІ string.  */
 string ip_to_str(SOCKADDR_STORAGE ip)
 {
 	string res = "";
@@ -184,18 +184,18 @@ string ip_to_str(SOCKADDR_STORAGE ip)
 	return res;
 }
 
-/*  Функция отправки ICMP пакета.  */
+/*  Р¤СѓРЅРєС†РёСЏ РѕС‚РїСЂР°РІРєРё ICMP РїР°РєРµС‚Р°.  */
 int send_packet(icmp_packet packet, int *stat_array)
 {
 	char buf[128];
 	int timeout = 5000;
 	int t_addr_size = sizeof(t_addr);
-	char time_buf[256];  //  Объявление буфера для приема данных.
-	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(int));  //  Установка тайм-аута (5000 мс.).
+	char time_buf[256];  //  РћР±СЉСЏРІР»РµРЅРёРµ Р±СѓС„РµСЂР° РґР»СЏ РїСЂРёРµРјР° РґР°РЅРЅС‹С….
+	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(int));  //  РЈСЃС‚Р°РЅРѕРІРєР° С‚Р°Р№Рј-Р°СѓС‚Р° (5000 РјСЃ.).
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-	//  Отправка пакета.
+	//  РћС‚РїСЂР°РІРєР° РїР°РєРµС‚Р°.
 	int send_res = sendto(sock, (char*)&packet, 40, 0, (SOCKADDR*)&t_addr, sizeof(t_addr));
 	if (send_res == SOCKET_ERROR)
 	{
@@ -205,14 +205,14 @@ int send_packet(icmp_packet packet, int *stat_array)
 		return 1;
 	}
 	cout << "Packet with " << send_res - 8 << " bytes was sent success." << endl;
-	stat_array[0] += 1;  //  +1 отправленный пакет.
+	stat_array[0] += 1;  //  +1 РѕС‚РїСЂР°РІР»РµРЅРЅС‹Р№ РїР°РєРµС‚.
 
-	//  Получение ответа.
+	//  РџРѕР»СѓС‡РµРЅРёРµ РѕС‚РІРµС‚Р°.
 	int recv_res = recvfrom(sock, (char*)&buf, (int)strlen(buf), NULL, (SOCKADDR*)&t_addr, &t_addr_size);
 	if (GetLastError() == 10060)
 	{
 		cout << "Request waiting interval exceeded." << endl;
-		stat_array[2] += 1;  //  +1 потерянный пакет.
+		stat_array[2] += 1;  //  +1 РїРѕС‚РµСЂСЏРЅРЅС‹Р№ РїР°РєРµС‚.
 	}
 	else if (recv_res == SOCKET_ERROR)
 	{
@@ -226,36 +226,36 @@ int send_packet(icmp_packet packet, int *stat_array)
 	}
 
 	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> elapsed_time = end - start;  //  Вычисление затраченного на получение пакета времени.
+	std::chrono::duration<double> elapsed_time = end - start;  //  Р’С‹С‡РёСЃР»РµРЅРёРµ Р·Р°С‚СЂР°С‡РµРЅРЅРѕРіРѕ РЅР° РїРѕР»СѓС‡РµРЅРёРµ РїР°РєРµС‚Р° РІСЂРµРјРµРЅРё.
 	if (recv_res > 0)
 	{
 		cout << "Recevied " << recv_res - 28 << " bytes from " << ip_to_str(t_addr) << " in " << (int)(elapsed_time.count() * 1000) << " ms." << endl;
 
-		//  Сбор статистики.
-		stat_array[1] += 1;  //  +1 полученный пакет.
-		if (stat_array[3] > (int)(elapsed_time.count() * 1000))  //  Минимальное время получения ответа.
+		//  РЎР±РѕСЂ СЃС‚Р°С‚РёСЃС‚РёРєРё.
+		stat_array[1] += 1;  //  +1 РїРѕР»СѓС‡РµРЅРЅС‹Р№ РїР°РєРµС‚.
+		if (stat_array[3] > (int)(elapsed_time.count() * 1000))  //  РњРёРЅРёРјР°Р»СЊРЅРѕРµ РІСЂРµРјСЏ РїРѕР»СѓС‡РµРЅРёСЏ РѕС‚РІРµС‚Р°.
 			stat_array[3] = (int)(elapsed_time.count() * 1000);
-		if (stat_array[4] < (int)(elapsed_time.count() * 1000))  //  Максимальное время получения ответа.
+		if (stat_array[4] < (int)(elapsed_time.count() * 1000))  //  РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ РІСЂРµРјСЏ РїРѕР»СѓС‡РµРЅРёСЏ РѕС‚РІРµС‚Р°.
 			stat_array[4] = (int)(elapsed_time.count() * 1000);
-		stat_array[5] += (int)(elapsed_time.count() * 1000);  //  Сумма времени отправки всех сообщений
-															  //  (для вычисления среднего времени).
+		stat_array[5] += (int)(elapsed_time.count() * 1000);  //  РЎСѓРјРјР° РІСЂРµРјРµРЅРё РѕС‚РїСЂР°РІРєРё РІСЃРµС… СЃРѕРѕР±С‰РµРЅРёР№
+															  //  (РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ СЃСЂРµРґРЅРµРіРѕ РІСЂРµРјРµРЅРё).
 	}
 
 	return 0;
 }
 
-/*  Функция создания и установки соеденения сокета.  */
+/*  Р¤СѓРЅРєС†РёСЏ СЃРѕР·РґР°РЅРёСЏ Рё СѓСЃС‚Р°РЅРѕРІРєРё СЃРѕРµРґРµРЅРµРЅРёСЏ СЃРѕРєРµС‚Р°.  */
 int run_socket(string ip_address)
 {
-	//  Начало работы с сетью.
+	//  РќР°С‡Р°Р»Рѕ СЂР°Р±РѕС‚С‹ СЃ СЃРµС‚СЊСЋ.
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
 		wprintf(L"WSAStartup failed: %d\n", iResult);
 		return 1;
 	}
 
-	//  Заполнение структуры хранящей информацию о адресе.
-	if ((ip_address[0] > 47) && (ip_address[0] < 58))  //  Если введен ip адрес.
+	//  Р—Р°РїРѕР»РЅРµРЅРёРµ СЃС‚СЂСѓРєС‚СѓСЂС‹ С…СЂР°РЅСЏС‰РµР№ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ Р°РґСЂРµСЃРµ.
+	if ((ip_address[0] > 47) && (ip_address[0] < 58))  //  Р•СЃР»Рё РІРІРµРґРµРЅ ip Р°РґСЂРµСЃ.
 	{
 		unsigned char ip_array[4];
 
@@ -266,10 +266,10 @@ int run_socket(string ip_address)
 		t_addr.__ss_pad1[4] = ip_array[2];
 		t_addr.__ss_pad1[5] = ip_array[3];
 	}
-	else  //  Если введен домен.
+	else  //  Р•СЃР»Рё РІРІРµРґРµРЅ РґРѕРјРµРЅ.
 	{
-		//  Получение ip адреса по имени хоста.
-		auto getaddr_res = getaddrinfo("google.com", NULL, &hints, &test_addr);  //  Адрес получаем в test_addr.
+		//  РџРѕР»СѓС‡РµРЅРёРµ ip Р°РґСЂРµСЃР° РїРѕ РёРјРµРЅРё С…РѕСЃС‚Р°.
+		auto getaddr_res = getaddrinfo("google.com", NULL, &hints, &test_addr);  //  РђРґСЂРµСЃ РїРѕР»СѓС‡Р°РµРј РІ test_addr.
 		if (getaddr_res != 0)
 		{
 			cout << "Getaddrinfo function failed." << endl;
@@ -278,13 +278,13 @@ int run_socket(string ip_address)
 			return 1;
 		}
 
-		//  Копирование полученного ip из структуры addrinfo* в sockaddr_storage.
+		//  РљРѕРїРёСЂРѕРІР°РЅРёРµ РїРѕР»СѓС‡РµРЅРЅРѕРіРѕ ip РёР· СЃС‚СЂСѓРєС‚СѓСЂС‹ addrinfo* РІ sockaddr_storage.
 		for (int i = 2; i < 6; i++)
 			t_addr.__ss_pad1[i] = test_addr->ai_addr->sa_data[i];
 		t_addr.ss_family = AF_INET;
 	}
 
-	//  Инициализация сокета.
+	//  РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ СЃРѕРєРµС‚Р°.
 	sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (sock == INVALID_SOCKET)
 	{
@@ -293,7 +293,7 @@ int run_socket(string ip_address)
 		return 1;
 	}
 
-	//  Установка соеденения по сокету.
+	//  РЈСЃС‚Р°РЅРѕРІРєР° СЃРѕРµРґРµРЅРµРЅРёСЏ РїРѕ СЃРѕРєРµС‚Сѓ.
 	if (connect(sock, (sockaddr*) &t_addr, sizeof(t_addr)) == SOCKET_ERROR)
 	{
 		cout << "Connection to " << ip_address << " failed." << endl;
@@ -309,7 +309,7 @@ int run_socket(string ip_address)
 
 
 
-/*  Основная функция main.  */
+/*  РћСЃРЅРѕРІРЅР°СЏ С„СѓРЅРєС†РёСЏ main.  */
 int main()
 {
 	int stat_array[6];
@@ -322,9 +322,9 @@ int main()
 	int ping_count;
 	string ip_addr;
 
-	//  Заполнение пакета соответствующими данными.
+	//  Р—Р°РїРѕР»РЅРµРЅРёРµ РїР°РєРµС‚Р° СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰РёРјРё РґР°РЅРЅС‹РјРё.
 	icmp_packet packet = fill_icmp_packet();
-	packet.control_sum = control_sum(packet);  //  Подсчет контрольной суммы
+	packet.control_sum = control_sum(packet);  //  РџРѕРґСЃС‡РµС‚ РєРѕРЅС‚СЂРѕР»СЊРЅРѕР№ СЃСѓРјРјС‹
 
 	cout << "Ping program.\n" << endl;
 	cout << "Enter the IP-Address or domain name to connect." << endl;
@@ -339,10 +339,10 @@ int main()
 	if (ping_count <= 0)
 		ping_count = 1;
 
-	//  Начало работы сокета.
+	//  РќР°С‡Р°Р»Рѕ СЂР°Р±РѕС‚С‹ СЃРѕРєРµС‚Р°.
 	run_socket(ip_addr);
 
-	//  Отправка пакетов.
+	//  РћС‚РїСЂР°РІРєР° РїР°РєРµС‚РѕРІ.
 
 	for (int i = 0; i < ping_count; i++)
 	{
@@ -352,7 +352,7 @@ int main()
 		packet.control_sum = control_sum(packet);
 	}
 
-	stat_array[5] /= ping_count;  //  Вычисление среднего времени получения пакета.
+	stat_array[5] /= ping_count;  //  Р’С‹С‡РёСЃР»РµРЅРёРµ СЃСЂРµРґРЅРµРіРѕ РІСЂРµРјРµРЅРё РїРѕР»СѓС‡РµРЅРёСЏ РїР°РєРµС‚Р°.
 
 	cout << "\nStatistics.\n\tPackets sent:\t" << stat_array[0] << "\n\tPackets received:\t" << stat_array[1] << "\n\tPacket loss:\t" << stat_array[2];
 	cout << " (" << stat_array[2] * (100 / stat_array[0]) << "%)" << endl;
